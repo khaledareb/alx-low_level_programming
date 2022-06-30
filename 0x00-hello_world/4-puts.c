ce8b54f9a2055e851f456947eaeea96e1a2db086
khaledareb <khaledareb@gmail.com>
2022-06-30 11:41:51 -0700

    asdasD

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,12 @@
 #include <stdio.h>
 
 /**
- *  * main - Prints "Programming is like building a multilingual
- *   *                puzzle, followed by a new line.
- *    *
- *     * Return: Always 0.
- *      */
+ *   * main - Prints a text
+ *     *
+ *       * Return: Always 0 (Success)
+ *         */
 int main(void)
 {
 		puts("\"Programming is like building a multilingual puzzle");
-
 			return (0);
 }
