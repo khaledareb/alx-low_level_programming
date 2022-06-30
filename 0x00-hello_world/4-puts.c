8e782c02966ec308e19b1176db9c45a117a6f6a7
khaledareb <97897761+khaledareb@users.noreply.github.com>
2022-06-30 22:31:13 +0300

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,8 +3,7 @@
 /**
  *   * main - Prints a text
  *     *
- *       * Return: Always 0 (Success)
- *         */
+ *       * Return: Always 0 (Success) * */
 int main(void)
 {
 		puts("\"Programming is like building a multilingual puzzle");
