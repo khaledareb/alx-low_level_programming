3a023133c9cf6924d79ec2938e205bfa78822889
khaledareb <97897761+khaledareb@users.noreply.github.com>
2022-07-05 16:05:33 +0300

    Update 3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,2 +1,18 @@
 #include "main.h"
 
+/**
+  * _islower - for checking og lowercase character
+  * @c: will be the character that will be checked
+  *
+  * Return: 1 for lowercase character or 0 for anything else
+  */
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122)
+	{
+		return (1);
+	}
+
+	return (0);
+}
+
