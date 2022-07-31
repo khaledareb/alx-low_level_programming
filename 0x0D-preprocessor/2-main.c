36569c9b72b8524691e7246c49e5a0ff084f98d7
khaledareb <97897761+khaledareb@users.noreply.github.com>
2022-08-01 01:36:46 +0300

    2-main

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,1 +1,12 @@
-#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Print the name of the file compiled from
+ * Return: 0;
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
