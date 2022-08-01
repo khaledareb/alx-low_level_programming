3fb5ce3c5d799c4cc71776c0965e26b9611ca557
khaledareb <97897761+khaledareb@users.noreply.github.com>
2022-08-01 15:38:38 +0300

    dog h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,16 +2,20 @@
 #define DOG_H
 
 /**
- * struct dog - structure definition of do
+ * struct dog - structure definition of a dog
  * @name: character string
- * @age : integer
- * @owner : character string
+ * @age: integer
+ * @owner: character string
  */
 typedef struct dog
 {
-
 	char *name;
 	float age;
-	char *owner
-} dog_t
+	char *owner;
+} dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 #endif
